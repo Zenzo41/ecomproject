77824698eab48fbba53deff677af7093301ed56b
Sonam Syangbo <syangbo.sonam88@gmail.com>
2023-08-21 22:25:47 +0545

    added products, categories and rendered in home

--- ecomapp/views.py
+++ ecomapp/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from .models import *
 
 # Create your views here.
 class HomeView(TemplateView):
     template_name = "home.html"
+
+    def get_context_data(self, **kwargs):
+        context =  super().get_context_data(**kwargs)
+        context['myname'] = "Zen Sama"
+        context['product_list'] = Product.objects.all()
+        return context
 
 class AboutView(TemplateView):
     template_name = "about.html"
