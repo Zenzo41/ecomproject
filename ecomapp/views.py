afac92ce2577f3b6b8859930129f4e8bb3686c4e
Sonam Syangbo <syangbo.sonam88@gmail.com>
2023-08-23 14:57:02 +0545

    cart functionality done

--- ecomapp/views.py
+++ ecomapp/views.py
@@ -45,19 +45,36 @@ class AddToCartView(TemplateView):
         product_obj = Product.objects.get(id = product_id)
         
         #check if cart exists:
-        cart_id = self.request.session.get(id = product_id)
+        cart_id = self.request.session.get("cart_id",None)
         if cart_id:
             cart_obj = Cart.objects.get(id=cart_id) # Old cart
-            this_product_in_cart = cart_obj.cartproducts_set.filter(product = product_obj)
+            this_product_in_cart = cart_obj.cartproduct_set.filter(product = product_obj)
 
             #item already exists in cart
+            if this_product_in_cart.exists():
+                cartproduct = this_product_in_cart.last()
+                cartproduct.quantity += 1
+                cartproduct.subtotal += product_obj.selling_price
+                cartproduct.save()
+                cart_obj.total += product_obj.selling_price
+                cart_obj.save()
+
+            #new item is added in cart
+            else:
+                cartproduct = CartProduct.objects.create(
+                    cart = cart_obj,product = product_obj , rate = product_obj.selling_price,quantity = 1, subtotal = product_obj.selling_price)
+                cart_obj.total += product_obj.selling_price
+                cart_obj.save()
+
         else:
             cart_obj = Cart.objects.create(total=0)
-            self.request.session['cart_id']= cart_obj.id # New Cart
-           
-        
-        #check if product already exists in cart:
+            self.request.session['cart_id']= cart_obj.id # New Cart 
+            cartproduct = CartProduct.objects.create(
+                    cart = cart_obj,product = product_obj , rate = product_obj.selling_price,quantity = 1, subtotal = product_obj.selling_price)
+            cart_obj.total += product_obj.selling_price
+            cart_obj.save()
 
+        return context
 
 class AboutView(TemplateView):
     template_name = "about.html"
