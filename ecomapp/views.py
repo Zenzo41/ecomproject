f6d502d8a7dd6369c688dd388e3df112cde4bad3
Sonam Syangbo <syangbo.sonam88@gmail.com>
2023-09-02 19:56:18 +0545

    customerorderdetail page not showing

--- ecomapp/views.py
+++ ecomapp/views.py
@@ -339,7 +339,7 @@ class AdminOrderDetailView(AdminRequiredMixin, DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["allstatus"] = ORDER_STATUS
+        context["allstatus"] = ORDER_STATUS 
         return context
 
 class AdminOrderListView(ListView):
