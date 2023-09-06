22eea267ed07fa5b1f9388d1fa3803a166c0710b
Sonam Syangbo <syangbo.sonam88@gmail.com>
2023-09-06 08:08:43 +0545

    khalti verify view added

--- ecomapp/views.py
+++ ecomapp/views.py
@@ -239,9 +239,39 @@ class KhaltiRequestView(View):
         }
         return render(request,"khaltirequest.html",context)
     
+
 class KhaltiVerifyView(View):
-    def get(self,request,*args,**kwargs):
-        data = {}
+    def get(self, request, *args, **kwargs):
+        token = request.GET.get("token")
+        amount = request.GET.get("amount")
+        o_id = request.GET.get("order_id")
+        print(token, amount, o_id)
+
+        url = "https://khalti.com/api/v2/payment/verify/"
+        payload = {
+            "token": token,
+            "amount": amount
+        }
+        headers = {
+            "Authorization": "Key test_secret_key_f59e8b7d18b4499ca40f68195a846e9b"
+        }
+
+        order_obj = Order.objects.get(id=o_id)
+
+        response = request.post(url, payload, headers=headers)
+        resp_dict = response.json()
+        if resp_dict.get("idx"):
+            success = True
+            order_obj.payment_completed = True
+            order_obj.save()
+        else:
+            success = False
+        data = {
+            "success": success
+        }
+        return JsonResponse(data)
+
+
 
 class CustomerRegistrationView(CreateView):
     template_name = "customerregistration.html"
